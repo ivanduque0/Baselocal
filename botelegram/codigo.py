5eed5c6c51f609c0a2038874da25263178fd5f03
ivanduque0 <98676748+ivanduque0@users.noreply.github.com>
2022-06-02 20:53:17 -0400

    adicion de botones
    se le agregaron botones al bot, ahora es mas facil usar el bot en situaciones donde no se pueda estar muy pendiente de donde presionar

--- botelegram/codigo.py
+++ botelegram/codigo.py
@@ -14,12 +14,22 @@ bot = telebot.TeleBot(token, parse_mode=None)
 razon=os.environ.get("RAZON_BOT")
 total=0
 CONTRATO=os.environ.get("CONTRATO")
-puertas = [
-    'entrada principal', 
-    'porton vehicular',
-    'puerta trasera'
+pulseaqui = [
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    'pulse aqui',
+    
 ]
-keyboard = Keyboa(items=puertas)
+
+keyboard = Keyboa(items=pulseaqui)
 
 
 def aperturaconcedida(nombref, fechaf, horaf, razonf, contratof, cedulaf, cursorf,connf):
@@ -33,104 +43,129 @@ def aperturadenegada(cursorf, connf):
     cursorf.execute('''UPDATE led SET onoff=2 WHERE onoff=0;''')
     connf.commit()
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['id'])
 def send_welcome(message):
     message.text
     chatid = message.from_user.id
-    bot.reply_to(message, f"Bienvenido, su ID es es: {chatid}")
-    bot.send_message(chat_id=chatid,text='Que acceso desea usar?', reply_markup=keyboard())
+    bot.reply_to(message, f"su ID es: {chatid}")
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    message.text
+    chatid = message.chat.id
+    bot.send_message(chat_id=chatid,text='Pulse cualquier boton', reply_markup=keyboard())
 
 @bot.callback_query_handler(func=lambda call: True)
 def manejador_seleccion(call):
 
-    if call.data == 'entrada principal':
-        #print('accionar')
-        diasusuario = []
-        cantidaddias = 0
-        contadoraux = 0
-        orden = 'abrir entrada principal'
-        chat_id = call.message.chat.id
-        #print(orden)
-        #print(chat_id)
-        #print(message)
+    chatid = call.message.chat.id
+    if call.data == 'pulse aqui':
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add("entrada principal")
+        markup.add("porton vehicular","puerta trasera")
+        bot.send_message(chat_id=chatid,text='Pulse cualquier boton', reply_markup=keyboard())
+        bot.send_message(chat_id=chatid,text='que acceso desea abrir?', reply_markup=markup)
+    
+@bot.message_handler(func=lambda message: True)
+def manejador_seleccion(message):
+	
+    chatid=message.chat.id
+    opciones = ["entrada principal","porton vehicular","puerta trasera"]
 
-        cursor.execute("SELECT * FROM web_usuarios where telegram_id='%s'", (chat_id,))
-        datosusuario = cursor.fetchall()
-        #print(datosusuario)
-        if len(datosusuario)!=0:
-            cedula=datosusuario[0][0]
-            nombre=datosusuario[0][1]
-            cursor.execute('SELECT * FROM web_horariospermitidos where cedula_id=%s', (cedula,))
-            horarios_permitidos = cursor.fetchall()
-            if horarios_permitidos != []:
-                tz = pytz.timezone('America/Caracas')
-                caracas_now = datetime.now(tz)
-                dia = caracas_now.weekday()
-                diahoy = dias_semana[dia]
-                for entrada, salida, _, dia in horarios_permitidos:
-                    diasusuario.append(dia)
-                cantidaddias = diasusuario.count(dia)
-                for entrada, salida, _, dia in horarios_permitidos:
-                    if dia==diahoy and cantidaddias==1:
-                        hora=str(caracas_now)[11:19]
-                        horahoy = datetime.strptime(hora, '%H:%M:%S').time()
-                        fecha=str(caracas_now)[:10]
-                        etapadia=1
-                        if entrada<salida:
-                            if horahoy >= entrada and horahoy <= salida:
-                                #print('entrada concedida')
-                                aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
-                                etapadiaapertura=1
-                            else:
-                                aperturadenegada(cursor, conn)
-                                #print('fuera de horario')
-                        if entrada>salida:
-                            if (horahoy>=entrada and horahoy <=ultimahora) or (horahoy>=primerahora and horahoy <= salida):
-                                #print('entrada concedida')
-                                aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
-                                etapadiaapertura=1
-                            else:
-                                aperturadenegada(cursor, conn)
-                                #print('fuera de horario')
-                    elif dia==diahoy and cantidaddias>1:
-                        hora=str(caracas_now)[11:19]
-                        horahoy = datetime.strptime(hora, '%H:%M:%S').time()
-                        fecha=str(caracas_now)[:10]
-                        etapadia=1
-                        if entrada<salida:
-                            if horahoy >= entrada and horahoy <= salida:
-                                #print('entrada concedida')
-                                aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
-                                etapadiaapertura=1
-                                contadoraux=0
-                            else:
-                                contadoraux = contadoraux+1
-                                if contadoraux == cantidaddias:
+    try:
+        opciones.index(message.text)
+        #print(message.text)
+        if message.text == 'entrada principal':
+            diasusuario = []
+            cantidaddias = 0
+            contadoraux = 0
+            chat_id = message.chat.id
+            cursor.execute("SELECT * FROM web_usuarios where telegram_id='%s'", (chat_id,))
+            datosusuario = cursor.fetchall()
+            #print(datosusuario)
+            if len(datosusuario)!=0:
+                cedula=datosusuario[0][0]
+                nombre=datosusuario[0][1]
+                cursor.execute('SELECT * FROM web_horariospermitidos where cedula_id=%s', (cedula,))
+                horarios_permitidos = cursor.fetchall()
+                if horarios_permitidos != []:
+                    tz = pytz.timezone('America/Caracas')
+                    caracas_now = datetime.now(tz)
+                    dia = caracas_now.weekday()
+                    diahoy = dias_semana[dia]
+                    for entrada, salida, _, dia in horarios_permitidos:
+                        diasusuario.append(dia)
+                    cantidaddias = diasusuario.count(dia)
+                    for entrada, salida, _, dia in horarios_permitidos:
+                        if dia==diahoy and cantidaddias==1:
+                            hora=str(caracas_now)[11:19]
+                            horahoy = datetime.strptime(hora, '%H:%M:%S').time()
+                            fecha=str(caracas_now)[:10]
+                            etapadia=1
+                            if entrada<salida:
+                                if horahoy >= entrada and horahoy <= salida:
+                                    #print('entrada concedida')
+                                    aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
+                                    etapadiaapertura=1
+                                else:
                                     aperturadenegada(cursor, conn)
-                                    contadoraux=0
-                        if entrada>salida:
-                            if (horahoy>=entrada and horahoy <=ultimahora) or (horahoy>=primerahora and horahoy <= salida):
-                                #print('entrada concedida')
-                                aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
-                                etapadiaapertura=1
-                                contadoraux=0
-                            else:
-                                contadoraux = contadoraux+1
-                                if contadoraux == cantidaddias:
+                                    #print('fuera de horario')
+                            if entrada>salida:
+                                if (horahoy>=entrada and horahoy <=ultimahora) or (horahoy>=primerahora and horahoy <= salida):
+                                    #print('entrada concedida')
+                                    aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
+                                    etapadiaapertura=1
+                                else:
                                     aperturadenegada(cursor, conn)
+                                    #print('fuera de horario')
+                        elif dia==diahoy and cantidaddias>1:
+                            hora=str(caracas_now)[11:19]
+                            horahoy = datetime.strptime(hora, '%H:%M:%S').time()
+                            fecha=str(caracas_now)[:10]
+                            etapadia=1
+                            if entrada<salida:
+                                if horahoy >= entrada and horahoy <= salida:
+                                    #print('entrada concedida')
+                                    aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
+                                    etapadiaapertura=1
                                     contadoraux=0
-                                #print('fuera de horario')
-                if etapadia==0 and etapadiaapertura==0:
-                    aperturadenegada(cursor, conn)
-                    #print('Dia no permitido')
-            if horarios_permitidos == []:
+                                else:
+                                    contadoraux = contadoraux+1
+                                    if contadoraux == cantidaddias:
+                                        aperturadenegada(cursor, conn)
+                                        contadoraux=0
+                            if entrada>salida:
+                                if (horahoy>=entrada and horahoy <=ultimahora) or (horahoy>=primerahora and horahoy <= salida):
+                                    #print('entrada concedida')
+                                    aperturaconcedida(nombre, fecha, horahoy, razon, CONTRATO, cedula, cursor,conn)
+                                    etapadiaapertura=1
+                                    contadoraux=0
+                                else:
+                                    contadoraux = contadoraux+1
+                                    if contadoraux == cantidaddias:
+                                        aperturadenegada(cursor, conn)
+                                        contadoraux=0
+                                    #print('fuera de horario')
+                    if etapadia==0 and etapadiaapertura==0:
+                        aperturadenegada(cursor, conn)
+                        #print('Dia no permitido')
+                if horarios_permitidos == []:
+                    aperturadenegada(cursor, conn) 
+                    #print('este usuario no tiene horarios establecidos')
+                diasusuario=[]    
+                
+            else:
                 aperturadenegada(cursor, conn) 
-                #print('este usuario no tiene horarios establecidos')
-            diasusuario=[]    
-            
-        else:
-            aperturadenegada(cursor, conn) 
+    except:
+        pass
+    finally:
+        
+        bot.send_message(chat_id=chatid,text='Pulse cualquier boton', reply_markup=keyboard())
+        markup = telebot.types.ReplyKeyboardMarkup()
+        markup.add("entrada principal")
+        markup.add("porton vehicular","puerta trasera")
+        bot.send_message(chat_id=chatid,text='que acceso desea abrir?', reply_markup=markup)
+
             
 
 while True:
